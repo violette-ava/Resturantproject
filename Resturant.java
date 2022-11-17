a8a6dd11e955bf01087d6a423f93c74a0c56c1e4
Ava Violette <avakviolette@yahoo.com>
2022-11-17 08:55:05 -0600

    Thursday in Class

--- Resturant.java
+++ Resturant.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Resturant {
@@ -6,10 +7,18 @@ public class Resturant {
     private static ArrayList<Food> menu;
     private static ArrayList<Orders> orders;
     
+    
 
     public static void main(String[] args){
+        ArrayList<Food> menu = new ArrayList<>();
+        menu.add(new Food("Pizza", 5));//DO THIS FOR ALL OF THEM
+        menu.add("Ice Cream");
+        menu.add("Salad");
+        menu.add("Sandwich");
+        menu.add("Cake");
        
         System.out.print("Welcome To My Resturant");
+
         
         int choice = 1;
         while(choice >0){
@@ -34,11 +43,11 @@ public class Resturant {
             else if(choice == 3){
                 optionThree();
             }
-            else if(choice == 4){
+            else if(choice == 4){ //USE TEAMS PROJECT
                 optionFour();
             }
 
-            public String
+           
 
             
 
@@ -61,8 +70,53 @@ public class Resturant {
     }
 
 
+    private static void optionThree() {
+        System.out.println("Thank you for choosing to Deliver");
+        System.out.print("Customer Name: ");
+        String CustomerName = sc.nextLine();
+        System.out.print("Address: ");
+        String addr =sc.nextLine();
+        System.out.print("Phone: ");
+        String phone = sc.nextLine();
+        ArrayList<Food> dishes = getmenu();//This function is inside  of Restaurant
+        DeliveryOrder  o = new DeliveryOrder(customer, addr, phone);
+        o.setDishes(dishes);
+        orders.add(o);
+        
+    }
+
+
+    private static void optionTwo() {
+        System.out.println("Thank you for choosing to Takeout");
+        System.out.println("What would you like to order?");
+        System.out.println(menu);
+        int choice = 1;
+        choice = sc.nextInt();
+        sc.nextLine();
+        switch(choice){
+            System.out.println(//TAKEOUT ORDER CODE");
+        }
+
+    }
+
+
     private static void optionOne() {
         System.out.println("Thank you for choosing to Dine-In");
+        System.out.println("What would you like to order?");
+        System.out.println(menu);
+        int choice = 1;
+        choice = sc.nextInt();
+        sc.nextLine();
+        switch (choice){
+            System.out.println(//TABLE ORDER CODE)
+            
+        }
+       
+        }
+
+    
+
+
 
     }
 
