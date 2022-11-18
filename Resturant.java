27017fffb27f42e4217dab7d1ab3f5b99f1ebc75
Ava Violette <avakviolette@yahoo.com>
2022-11-18 12:32:17 -0600

    SWAG4

--- Resturant.java
+++ Resturant.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Resturant {
-    private static ArrayList<Order>  orders = new ArrayList<>();
+    private static ArrayList<Order> orders = new ArrayList<>();
     private static ArrayList<Food> menu = new ArrayList<>();
     private static Scanner sc = new Scanner(System.in);
     String[] PossibleStatuses;
 
-   
+    public static void main(String[] args) {
 
 
-    
-
-    
-    
-
-    public static void main(String[] args){
+        
+        menu.add(new Food("1) Pizza", 5));
+        menu.add(new Food("2) Ice Cream", 3));
+        menu.add(new Food("3) Salad", 7));
+        menu.add(new Food("4) Sandwich", 12));
+        menu.add(new Food("5) Cake", 8));
 
         int choice = 1;
-        while(choice >0){
+        while (choice > 0) {
             System.out.println();
             System.out.println();
             System.out.println("Welcome");
@@ -33,192 +33,157 @@ public class Resturant {
             choice = sc.nextInt();
             sc.nextLine();
 
-
-
-
-            switch(choice){ 
+            switch (choice) {
                 case 1:
-                System.out.println("Customer Name");
-                String customer = sc.nextLine();
-                System.out.println("Number of guests?");
-                int numOfGuests = sc.nextInt();
-                sc.nextLine();
-                int tableNumber = 1;
-                break;
-                case 2:
-                System.out.println("Customer Name");
-                customer = sc.nextLine();
-                break;
-                case 3:
-                System.out.print("Customer Name: ");
-                customer = sc.nextLine();
-                System.out.print("Address: ");
-                String addr =sc.nextLine();
-                System.out.print("Phone: ");
-                String phone = sc.nextLine();
-                ArrayList<Food> dishes = getDishes();
-                DeliveryOrder  dl = new DeliveryOrder(customer, addr, phone);
-                dl.setDishes(dishes);
-                orders.add(dl);
-
-                break;
-                case 4:
-                choice = 4;
-                while(choice <1 || choice > 3){
-                    System.out.println("Sort by:  1.Order number 2.Type 3.Status");
-                    choice = sc.nextInt();
-                    sc.nextLine(); 
-                }    
-                listOrders(choice);
-                break;
-                case 5:
-                choice = 5;
-                System.out.println("Update Order:");
-                System.out.println("1) Update Status");
-                System.out.println("2) Add Dish");
-                System.out.println("3) Remove Dish");
-                System.out.println("4) Cancel Order");
-                System.out.println(); 
-                System.out.println("Choice: ");
-                int choice2 = sc.nextInt();
-                sc.nextLine();
-
-                for(int i =0; i<orders.size();i++){
-                    System.out.println(i+" "+orders.get(i));
-                }
-                System.out.println("Choice:");
-                int on = sc.nextInt();
-                sc.nextLine();
-                Order o = orders.get(on);
-
-
-                if (choice2 == 1) {
-                    optionUpdateStatus(o);
-                } else if (choice2 == 2) {
-                    optionAddDish(o);
-                } else if (choice2 == 3) {
-                    optionRemoveDish(o);
-                } else if (choice2 == 4) {
-                    optionCancelOrder(o);
-                }
-            }
-
-        }
-
-
-
-            
-
-            ArrayList<Food> menu = new ArrayList<>();
-            menu.add(new Food("Pizza", 5));
-            menu.add(new Food("Ice Cream", 3));
-            menu.add(new Food("Salad",7));
-            menu.add(new Food("Sandwich",12));
-            menu.add(new Food("Cake",8));}
-
-    
-
-
-
-
-
-            public static ArrayList<Food> getDishes(){
-                ArrayList<Food>  Dishes =new ArrayList<>();
-                int ans = 1;
-                while(ans !=0){
-                    for(int i = 0; i < menu.size();i++){
-                        System.out.println(i+1+" "+menu.get(i));
-                    }
-                    System.out.println("Enter Choice (-1 to exit): ");
-                    System.out.println(menu);
-                    ans = sc.nextInt();
+                    System.out.println("Customer Name");
+                    String customer = sc.nextLine();
+                    System.out.println("Number of guests?");
+                    int numOfGuests = sc.nextInt();
                     sc.nextLine();
-                    if(ans > 0 && ans <=menu.size()){
-                        menu.add(menu.get(ans));
+                    TableOrder ta = new TableOrder(customer, numOfGuests, numOfGuests);
+                    orders.add(ta);
+                    int tableNumber = 1;
+                    break;
+                case 2:
+                    System.out.println("Customer Name");
+                    customer = sc.nextLine();
+                    TakeoutOrder to = new TakeoutOrder(customer);
+                    orders.add(to);
+                    break;
+                case 3:
+                    System.out.print("Customer Name: ");
+                    customer = sc.nextLine();
+                    System.out.print("Address: ");
+                    String addr = sc.nextLine();
+                    System.out.print("Phone: ");
+                    String phone = sc.nextLine();
+                    ArrayList<Food> dishes = getDishes();
+                    DeliveryOrder dl = new DeliveryOrder(customer, addr, phone);
+                    dl.setDishes(dishes);
+                    orders.add(dl);
+
+                    break;
+                case 4:
+                    choice = 4;
+                    while (choice < 1 || choice > 3) {
+                        System.out.println("Sort by:  1.Order number 2.Type 3.Status");
+                        choice = sc.nextInt();
+                        sc.nextLine();
                     }
-                    else if(ans== -1){
-                        break;
-                    }else{
-                        System.out.println("Invalid choice");
+                    listOrders(choice);
+                    break;
+                case 5:
+                    choice = 5;
+                    System.out.println("Update Order:");
+                    System.out.println("1) Update Status");
+                    System.out.println("2) Add Dish");
+                    System.out.println("3) Remove Dish");
+                    System.out.println("4) Cancel Order");
+                    System.out.println();
+                    System.out.println("Choice: ");
+                    int choice2 = sc.nextInt();
+                    sc.nextLine();
+
+                    for (int i = 0; i < orders.size(); i++) {
+                        System.out.println(i + " " + orders.get(i));
                     }
-                }
-                return Dishes;
+                    System.out.println("Choice:");
+                    int on = sc.nextInt();
+                    sc.nextLine();
+                    Order o = orders.get(on);
+
+                    if (choice2 == 1) {
+                        optionUpdateStatus(o);
+                    } else if (choice2 == 2) {
+                        optionAddDish(o);
+                    } else if (choice2 == 3) {
+                        optionRemoveDish(o);
+                    } else if (choice2 == 4) {
+                        optionCancelOrder(o);
+                    }
             }
 
-            
+        }
 
-
-
-
-
-    
-        
-
-    
-
- 
-
-private static String getType(Order o) {
-    if(o instanceof TakeoutOrder ){
-        return "TakeoutOrder";
-    }
-    else if(o instanceof DeliveryOrder){
-        return "DeliveryOrder";
-    }else if(o instanceof TableOrder){
-        return "TableOrder";
-    }
-    else{
-        return "";
     }
 
-
-                
-}
-
-public static void listOrders(int n){                   
-    if(n == 1){
-    }else if(n == 2){
-        HashSet<String> orderTypes = new HashSet<>();
-        for(Order o: orders)
-            orderTypes.add(getType(o));
-
-        for(String orderType:  orderTypes){
-            for(Order o: orders){
-                if(getType(o).equals(orderType)){
-                    System.out.println(o);
-                }
+    public static ArrayList<Food> getDishes() {
+        ArrayList<Food> Dishes = new ArrayList<>();
+        int ans = 1;
+        while (ans != 0) {
+            for (int i = 0; i < menu.size(); i++) {
+                System.out.println(i + 1 + " " + menu.get(i));
+            }
+            System.out.println("Enter Choice (-1 to exit): ");
+            System.out.println(menu);
+            ans = sc.nextInt();
+            sc.nextLine();
+            if (ans > 0 && ans <= menu.size()) {
+                menu.add(menu.get(ans));
+            } else if (ans == -1) {
+                break;
+            } else {
+                System.out.println("Invalid choice");
             }
         }
-
-
-    }else if(n == 3){ //Print order by status
-        for(Order o: orders){
-            System.out.println("Status:" + o.setStatus);
-        }
-       
+        return Dishes;
     }
-}
 
-public String[] getPossibleStatuses(){
-    String[] s = {"Placed", "Cooking", "Canceled"};
-    return s;
-}
+    private static String getType(Order o) {
+        if (o instanceof TakeoutOrder) {
+            return "TakeoutOrder";
+        } else if (o instanceof DeliveryOrder) {
+            return "DeliveryOrder";
+        } else if (o instanceof TableOrder) {
+            return "TableOrder";
+        } else {
+            return "";
+        }
 
-public void setStatus(String s){
-   String status =s;
-}
+    }
 
+    public static void listOrders(int n) {
+        if (n == 1) {
+            for (Order o : orders) {
+                System.out.println(o);
+            }
+        } else if (n == 2) {
+            HashSet<String> orderTypes = new HashSet<>();
+            for (Order o : orders)
+                orderTypes.add(getType(o));
 
+            for (String orderType : orderTypes) {
+                for (Order o : orders) {
+                    if (getType(o).equals(orderType)) {
+                        System.out.println(o);
+                    }
+                }
+            }
 
+        } else if (n == 3) { // Print order by status
+            for (Order o : orders) {
+                System.out.println("Status:" + o.setStatus);
+            }
 
+        }
+    }
+
+    public String[] getPossibleStatuses() {
+        String[] s = { "Placed", "Cooking", "Canceled" };
+        return s;
+    }
+
+    public void setStatus(String s) {
+        String status = s;
+    }
 
     private static void optionUpdateStatus(Order o) {
-       
-        System.out.print("Updating Status...");
-        String[]  statuses  = o.getPossibleStatuses();
-        
-        for(int i=0; i<statuses.length;i++){
-            System.out.println(i +" "+statuses[i]);
+
+        System.out.println("Updating Status...");
+        String[] statuses = o.getPossibleStatuses();
+
+        for (int i = 0; i < statuses.length; i++) {
+            System.out.println(i + " " + statuses[i]);
         }
         int choice = sc.nextInt();
         sc.nextLine();
@@ -227,36 +192,30 @@ public void setStatus(String s){
 
     }
 
-    private static void optionAddDish(Order o){
+    private static void optionAddDish(Order o) {
         System.out.println("Add a Dish");
         System.out.println(menu);
-        int menuItem  = sc.nextInt();
+        int menuItem = sc.nextInt();
         sc.nextLine();
 
         o.addDish(menu.get(menuItem));
-      
+
     }
 
-    private static void optionRemoveDish(Order o){
+    private static void optionRemoveDish(Order o) {
         System.out.println("Remove a Dish");
         System.out.println(menu);
-        int menuItem  = sc.nextInt();
+        int menuItem = sc.nextInt();
         sc.nextLine();
 
         o.removeDish(menu.get(menuItem));
-       
+
     }
 
-    private static Order optionCancelOrder(Order o){
+    private static Order optionCancelOrder(Order o) {
         System.out.println("Canceling Order");
         o.setStatus = "Canceled";
         return o;
     }
 
-
-            
-
-
-
-   
 }
